d5dba34ad472552c607e5fa872613c2750d099f5
M-Ali-Ramazani <112561860+ramazanima@users.noreply.github.com>
2023-12-08 14:13:31 -0500

    Update script.js
    completed button roll function and implemented button hold

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 // functions
 function changeDice() {}
 // selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
 const diceEl = document.querySelector('.dice');
@@ -11,9 +13,12 @@ const btnHold = document.querySelector('.btn--hold');
 const currScore0El = document.querySelector('#current--0');
 const currScore1El = document.querySelector('#current--1');
 
-let randomNumber = (score0El.textContent = 0);
+score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+const totalScores = [0, 0];
+let activePlayer = 0;
 
 let currentScore = 0;
 btnRoll.addEventListener('click', function () {
@@ -26,8 +31,28 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     // Add dice to current score
     currentScore += dice;
-    currScore0El.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
     // switch to another player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
+});
+
+btnHold.addEventListener('click', function () {
+  let index = activePlayer;
+  totalScores[index] += currentScore;
+  document.getElementById(`score--${activePlayer}`).textContent = Number(
+    totalScores[index]
+  );
+  currentScore = 0;
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
 });
