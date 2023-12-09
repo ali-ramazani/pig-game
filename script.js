b23b73e8491cc2b3469320c6e888fd94d111110e
M-Ali-Ramazani <112561860+ramazanima@users.noreply.github.com>
2023-12-09 01:12:15 -0500

    Update script.js
    Added Button Hold Functionality and First Page Overlay

--- script.js
+++ script.js
@@ -13,6 +13,20 @@ const btnHold = document.querySelector('.btn--hold');
 const currScore0El = document.querySelector('#current--0');
 const currScore1El = document.querySelector('#current--1');
 
+let maximumWinningScore = 0;
+
+const maxWinningScoreBtn = document.querySelector('.maxWinningScoreTaken');
+maxWinningScoreBtn.addEventListener('click', function () {
+  const maxScore = document.querySelector('#maxWinningScore').value;
+  const player1Name = document.querySelector('#player1-name').value;
+  const player2Name = document.querySelector('#player2-name').value;
+  document.querySelector('#name--0').textContent = player1Name;
+  document.querySelector('#name--1').textContent = player2Name;
+
+  maximumWinningScore = maxScore;
+  document.querySelector('.home-screen').classList.add('novisibility');
+});
+
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
@@ -50,9 +64,34 @@ btnHold.addEventListener('click', function () {
   document.getElementById(`score--${activePlayer}`).textContent = Number(
     totalScores[index]
   );
-  currentScore = 0;
-  document.getElementById(`current--${activePlayer}`).textContent = 0;
-  activePlayer = activePlayer === 0 ? 1 : 0;
-  player0El.classList.toggle('player--active');
-  player1El.classList.toggle('player--active');
+
+  if (totalScores[activePlayer] >= maximumWinningScore) {
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.remove('player--active');
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.add('player--winner');
+    document.querySelector('.dice').classList.add('hidden');
+    document.querySelector('.btn--roll').classList.add('hidden');
+    document.querySelector('.btn--hold').classList.add('hidden');
+    document.querySelector('.overlay').classList.remove('novisibility');
+    document.querySelector('.overlay').textContent = `Player ${
+      activePlayer + 1
+    } won`;
+  } else {
+    // change player
+    currentScore = 0;
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+  }
+});
+
+const value = document.querySelector('#value');
+const input = document.querySelector('#maxWinningScore');
+value.textContent = input.value;
+input.addEventListener('input', event => {
+  value.textContent = event.target.value;
 });
